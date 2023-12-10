87c23d8fe594b6d699d32d8eef1a344618019625
korilium <ignace.decocq@hotmail.com>
2023-12-10 22:29:24 +0100

    gettin got know package

--- sandbox/Sandbox1.jl
+++ sandbox/Sandbox1.jl
@@ -1,6 +1,36 @@
-LifeSimulator  
+using LifeSimulator  
+using Dates
+
+
+include("../src/LifeSimulator.jl")
 
 
 # Select the model you want to use, with parameters you like.
 # Read the documentation with `?LifelibBasiclife` (help mode) in the REPL.
 model = LifelibBasiclife(inflation_rate = 0.03)
+
+
+model.mortality
+model.acquisition_cost
+
+policies = [
+    PolicySet(Policy(term = Year(20), age = Year(20), premium = 200_000), 100),
+    PolicySet(Policy(term = Year(20), age = Year(45), premium = 600_000), 80),
+    PolicySet(Policy(term = Year(10), age = Year(70), premium = 400_000), 50),
+]
+
+n= 1
+
+simulate(model, policies, n) do events 
+    cf = CashFlow(events, model)
+    display(cf)
+end ;
+
+test = TabularMortality()
+
+
+test.rates[1,1]
+
+
+
+estimate_premiums(model, policies,n)
