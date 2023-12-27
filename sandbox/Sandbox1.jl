27e790e7c7105c223796e0ad816a8b06e5489797
korilium <ignace.decocq@hotmail.com>
2023-12-27 23:00:01 +0100

    tests

--- sandbox/Sandbox1.jl
+++ sandbox/Sandbox1.jl
@@ -1,9 +1,9 @@
-using LifeSimulator  
 using Dates
-
+using LifeSimulator
 
 include("../src/LifeSimulator.jl")
 
+using LifeSimulator
 
 # Select the model you want to use, with parameters you like.
 # Read the documentation with `?LifelibBasiclife` (help mode) in the REPL.
@@ -13,13 +13,17 @@ model = LifelibBasiclife(inflation_rate = 0.03)
 model.mortality
 model.acquisition_cost
 
+model.lapse
+model.load_premium_rate
+
+
 policies = [
     PolicySet(Policy(term = Year(20), age = Year(20), premium = 200_000), 100),
     PolicySet(Policy(term = Year(20), age = Year(45), premium = 600_000), 80),
     PolicySet(Policy(term = Year(10), age = Year(70), premium = 400_000), 50),
 ]
 
-n= 1
+n= 100
 
 simulate(model, policies, n) do events 
     cf = CashFlow(events, model)
